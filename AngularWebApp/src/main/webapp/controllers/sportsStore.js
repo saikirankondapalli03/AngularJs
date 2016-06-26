bab9ea5650e362d8c1bc67267ef0cdae23b4ce75
sai <saikirankondapalli03@gmail.com>
2016-06-26 17:40:56 +0530

    1. used service $http for loading the data from restful end point 2. Used deplyd for storing json data

--- AngularWebApp/src/main/webapp/controllers/sportsStore.js
+++ AngularWebApp/src/main/webapp/controllers/sportsStore.js
@@ -1,4 +1,4 @@
-angular.module("sportsStore").controller("sportsStoreCtrl", function($scope) {
+/*angular.module("sportsStore").controller("sportsStoreCtrl", function($scope) {
 	$scope.data = {
 		products : [ {
 			name : "Product #1",
@@ -43,38 +43,28 @@ angular.module("sportsStore").controller("sportsStoreCtrl", function($scope) {
 		} ]
 	};
 });
-
-
-/* 
-
-angular.module("sportsStore")
-.constant("dataUrl", "http://localhost:5500/products")
-.constant("orderUrl", "http://localhost:5500/orders")
-.controller("sportsStoreCtrl", function ($scope, $http, $location,
-dataUrl, orderUrl, cart) {
-$scope.data = {
-};
-$http.get(dataUrl)
-.success(function (data) {
-$scope.data.products = data;
-})
-.error(function (error) {
-$scope.data.error = error;
-});
-$scope.sendOrder = function (shippingDetails) {
-var order = angular.copy(shippingDetails);
-order.products = cart.getProducts();
-$http.post(orderUrl, order)
-.success(function (data) {
-$scope.data.orderId = data.id;
-cart.getProducts().length = 0;
-})
-.error(function (error) {
-$scope.data.orderError = error;
-}).finally(function () {
-$location.path("/complete");
-});
-}
-});
-
 */
+angular.module("sportsStore")
+.constant("orderUrl", "http://localhost:5500/orders")
+.constant("dataUrl",
+		"http://localhost:5500/products").controller("sportsStoreCtrl",
+		function($scope, $http, $location, dataUrl,orderUrl, cart) {
+			$scope.data = {};
+			$http.get(dataUrl).success(function(data) {
+				$scope.data.products = data;
+			}).error(function(error) {
+				$scope.data.error = error;
+			});
+			$scope.sendOrder = function(shippingDetails) {
+				var order = angular.copy(shippingDetails);
+				order.products = cart.getProducts();
+				$http.post(orderUrl, order).success(function(data) {
+					$scope.data.orderId = data.id;
+					cart.getProducts().length = 0;
+				}).error(function(error) {
+					$scope.data.orderError = error;
+				}).finally(function() {
+					$location.path("/complete");
+				});
+			}
+		});
